bf7eff6c2b8ca59a338d658c6431c24db3883f25
manar.afifi <manar.afifi@etu.estia.fr>
2025-04-15 11:48:43 +0200

    Navbar + style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,48 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function Navbar() {
-    return <h1>Navbar goes here</h1>;
+    return (
+        <Header>
+            <Nav>
+                <LogoContainer>
+                    <Logo src="/logo192.png" alt="React logo" />
+                    <Brand>ReactFacts</Brand>
+                </LogoContainer>
+                <Subtitle>React Course - Project 1</Subtitle>
+            </Nav>
+        </Header>
+    );
 }
+
+const Header = styled.header`
+  background-color: #21222A;
+  padding: 20px 36px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Logo = styled.img`
+  width: 40px;
+`;
+
+const Brand = styled.h1`
+  color: #61DAFB;
+  font-size: 1.5rem;
+`;
+
+const Subtitle = styled.h4`
+  color: #DEEBF8;
+  font-weight: 400;
+`;
+
